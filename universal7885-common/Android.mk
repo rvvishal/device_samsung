a2497895354c81ed1b05be9153a60e3fb14a7401
Gabriel260 <adeilmazk@gmail.com>
2021-02-28 15:11:05 -0300

    exynos7885: hotfix
    Signed-off-by: Gabriel260 <adeilmazk@gmail.com>

--- universal7885-common/Android.mk
+++ universal7885-common/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter m20lte m30lte a40 a30, $(TARGET_DEVICE)),)
+ifneq ($(filter a10 a20, $(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
