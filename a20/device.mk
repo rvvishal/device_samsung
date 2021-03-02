6a7c58b39cd397296728ca4c3e83bd01622c650f
Gabriel260 <adeilmazk@gmail.com>
2021-03-02 15:40:00 -0300

    exynos7885: add a20 support
    Signed-off-by: Gabriel260 <adeilmazk@gmail.com>

---
+++ a20/device.mk
@@ -0,0 +1,4 @@
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Inherit common device configuration
+$(call inherit-product, device/samsung/universal7885-common/universal7885-common.mk)
---
+++ a20/lineage_a20.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, device/samsung/a20/full_a20.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_a20
