4359fc3b45dcd116fa7609f8fe5195cad7a0512f
chatur27 <jasonbright2709@gmail.com>
2021-04-25 11:12:38 +0400

    Add some missing stuffs

--- a10/lineage_a10.mk
+++ a10/lineage_a10.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/a10/full_a10.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_go_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Build type: official
 SAKURA_OFFICIAL := true
@@ -9,7 +9,8 @@ SAKURA_MAINTAINER := Chatur
 
 # Sakura gapps
 TARGET_GAPPS_ARCH := arm64
-SAKURA_GAPPS := false
+#SAKURA_GAPPS := false
+SAKURA_BUILD_TYPE=microg
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED := true
@@ -19,5 +20,7 @@ TARGET_BOOT_ANIMATION_RES := 720
 
 # Use OnePlus launcher
 # SAKURA_OPLAUNCHER := true
+
+TARGET_USES_BLUR := true
 
 PRODUCT_NAME := lineage_a10
