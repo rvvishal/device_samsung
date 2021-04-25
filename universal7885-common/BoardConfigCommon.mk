615e3d7e7a4a04f9c9199e56e2e3447e537e8a7f
chatur27 <jasonbright2709@gmail.com>
2021-04-25 16:36:16 +0400

    Set up Sakura for A20e

--- universal7885-common/BoardConfigCommon.mk
+++ universal7885-common/BoardConfigCommon.mk
@@ -57,8 +57,8 @@ TARGET_KERNEL_ARCH := arm64
 #KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin
 BOARD_KERNEL_SEPARATED_DTBO := true
 #TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_PREBUILT_KERNEL := device/samsung/a10/prebuilt/Image
-BOARD_PREBUILT_DTBOIMAGE := device/samsung/a10/prebuilt/dtbo.img
+TARGET_PREBUILT_KERNEL := device/samsung/a20e/prebuilt/Image
+BOARD_PREBUILT_DTBOIMAGE := device/samsung/a20e/prebuilt/dtbo.img
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
--- universal7885-common/universal7885-common.mk
+++ universal7885-common/universal7885-common.mk
@@ -1,4 +1,4 @@
-DEVICE := a10
+DEVICE := a20e
 DEVICE_PATH := device/samsung/$(DEVICE)
 
 # Call proprietary blob setup
@@ -19,12 +19,12 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 1520
+TARGET_SCREEN_HEIGHT := 1560
 TARGET_SCREEN_WIDTH := 720
 
 # FastCharge
-#PRODUCT_PACKAGES += \
-#    lineage.fastcharge@1.0-service.samsung
+PRODUCT_PACKAGES += \
+    lineage.fastcharge@1.0-service.samsung
 
 # Init scripts
 PRODUCT_PACKAGES += \
@@ -55,6 +55,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
     frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
     frameworks/native/data/etc/android.software.controls.xml:system/etc/permissions/android.software.controls.xml
 
